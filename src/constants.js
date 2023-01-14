b38e8824e810fe20ebcb612aef5adf833a8d99d0
harsh116 <71572299+harsh116@users.noreply.github.com>
2023-01-14 13:16:23 +0530

    changed render to glitch;

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 // export const HOST = "http://localhost:8081";
 // export const HOST = "http://192.168.29.234:8081";
 
-export const HOST = "https://puppeteer-test-zoqc.onrender.com";
+// export const HOST = "https://puppeteer-test-zoqc.onrender.com";
+export const HOST='https://plausible-wobbly-sock.glitch.me'
